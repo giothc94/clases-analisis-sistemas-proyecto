79c7d78705e4b29b396dfdfa41bf946e1984fe92
Geovanny G. Arguello <gio666nb@gmail.com>
2020-11-20 12:50:01 -0500

    Resolution message

--- server/lib/mongodb.js
+++ server/lib/mongodb.js
@@ -3,6 +3,16 @@ const MongoClient = require("mongodb").MongoClient;
 const URL = "mongodb://localhost:27017";
 const dbName = "ecommerce";
 
+
+/**
+ * El problema era asincrónico, el callback que se le pasa a MongoClient.connect()
+ * no se resolvía al tiempo correcto en cuanto a asincronismo, por eso en esta función tuve que implementar
+ * el tradicional new Promise.
+ * 
+ * En el archivo crudMongo las funciones asincronas funcionan correctamente sin new Promise, 
+ * el problema se presenta solo aquí
+ * 
+ */
 const Mongo = async () =>
   new Promise((resolve, reject) => {
     MongoClient.connect(URL, (error, client) => {
