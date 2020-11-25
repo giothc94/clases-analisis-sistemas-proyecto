6d95e4d8b7c5c52b7951f5d995cca824254147c3
Geovanny G. Arguello <gio666nb@gmail.com>
2020-11-25 11:43:22 -0500

    add middlewares cors express-fileupload

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -8,10 +10,12 @@ const productsRouter = require('./routes/products');
 
 var app = express();
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(fileUpload())
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- server/routes/products.js
+++ server/routes/products.js
@@ -1,28 +1,33 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const MongoService = require('../services/crudMongo');
+const MongoService = require("../services/crudMongo");
 
-const response = require('../utils/response');
+const response = require("../utils/response");
 
 router
-  .get("/list", (req, res) => {
-
-  })
+  .get("/list", (req, res) => {})
   .post("/create", async (req, res) => {
     try {
-      let data = req.body
-      let result = await MongoService.create(data)
-      response(res, 201, result, "Usuario creado")
+      let data = req.body;
+      let result = await MongoService.create(data);
+      response(res, 201, result, "Usuario creado");
     } catch (error) {
-      console.log(error)
-      response(res, 500, {}, "Internal server error")
+      console.log(error);
+      response(res, 500, {}, "Internal server error");
     }
   })
-  .put("/update", (req, res) => {
-
+  .post("/upload", (req, res) => {
+    console.log(req.files);
+    let file = req.files.imagenProducto
+    file.mv(
+      `C:\\Users\\gio66\\Desktop\\Javascript\\proyecto\\server\\upload\\`+file.name,
+      (error) => {
+        console.log(error);
+      }
+    );
+    response(res, 200, {}, "Imagen cargada");
   })
-  .delete("/delete", (req, res) => {
+  .put("/update", (req, res) => {})
+  .delete("/delete", (req, res) => {});
 
-  })
-
-module.exports = router
+module.exports = router;
